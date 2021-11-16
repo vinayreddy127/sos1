1b4b01c61c6835abb8359a16e347060b1ad601ce
Vinay Reddy <vinay.reddy@team.telstra.com>
2021-11-16 16:05:31 +0530

    Initial Commit

---
+++ sos1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sos1
+//
+//  Created by Vinay Reddy on 16/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
